4012e6775adf7e4c349378c73dc432b5e13c1d14
Asara Khatun <asarakhatun17@gmail.com>
2026-09-16 02:32:06 +0000

    Added user input feature to script

---
+++ first_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello Boss! Welcome to the world of DevOps."
+echo "Aapka naam kya hai?"
+read user_name
+echo "Nice to meet you, $user_name! Chalo aaj kuch naya automate karte hain."
